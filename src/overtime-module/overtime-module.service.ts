98c33bd9d7d305e3a8babcb8a4621f06e07e7986
Veerapat Purthai <pu.veerapat@mpmth.com>
2025-11-24 10:38:48 +0700

    add: create OT API route

--- src/overtime-module/overtime-module.service.ts
+++ src/overtime-module/overtime-module.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import {CreateOvertimeRequestDto,UpdateOvertimeStatusDto} from './dto/dto';
 
 @Injectable()
-export class OvertimeModuleService {}
+export class OvertimeModuleService {
+    updateOvertimeStatus(id: string, dto: UpdateOvertimeStatusDto) {
+      throw new Error('Method not implemented.');
+    }
+    getTeamRequests() {
+      return "This action returns all overtime requests";
+    }
+
+    createOvertimeRequest(dto: CreateOvertimeRequestDto) {
+        throw new Error('Method not implemented.');
+    }
+
+    getrequestOvertime() {
+        return "This action returns all overtime requests";
+    }
+
+    getOvertimeById(id: string) {
+        throw new Error('Method not implemented.');
+    }
+}
